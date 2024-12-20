377c1235c6d6ae4c7f5aef9f26be57637fa9cc17
Tio Andrew <tio10andrew20yosua@gmail.com>
2024-12-21 00:43:48 +0700

    first commit

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+// filepath: /c:/laragon/www/Laravel-library-management-system-main/api/index.php
+
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
